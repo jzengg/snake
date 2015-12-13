b6ae5276bece1811c8c0acbac919a199782922cd
Jimmy Zeng <jzengg@gmail.com>
2015-12-13 15:52:56 -0500

    wrap coord in iife

--- js/coord.js
+++ js/coord.js
@@ -1,14 +1,18 @@
-var Coord = window.SnakeGame.Coord = function (arr) {
-  this.row = arr[0];
-  this.col = arr[1];
-};
+(function () {
 
-Coord.prototype.plus = function (otherCord) {
-  var newRow = this.row + otherCord.row;
-  var newCol = this.col + otherCord.col;
-  return new Coord([newRow, newCol]);
-};
+  var Coord = window.SnakeGame.Coord = function (arr) {
+    this.row = arr[0];
+    this.col = arr[1];
+  };
 
-Coord.prototype.equals = function (otherCoord) {
-  return (this.row === otherCoord.row) && (this.col === otherCoord.col);
-};
+  Coord.prototype.plus = function (otherCord) {
+    var newRow = this.row + otherCord.row;
+    var newCol = this.col + otherCord.col;
+    return new Coord([newRow, newCol]);
+  };
+
+  Coord.prototype.equals = function (otherCoord) {
+    return (this.row === otherCoord.row) && (this.col === otherCoord.col);
+  };
+
+})();
